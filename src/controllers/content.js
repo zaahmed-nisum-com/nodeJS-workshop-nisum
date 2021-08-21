9e427fb7d669dc329c94d58407a0c0a4a87babc8
Zain Ahmed <>
2021-08-21 13:28:58 +0500

    update

--- src/controllers/content.js
+++ src/controllers/content.js
@@ -1,5 +1,6 @@
 const contentModle = require("../models/content");
 const userModle = require("../models/user");
+const fs = require('fs')
 const mongoose = require("mongoose");
 
 module.exports = {
@@ -57,13 +58,42 @@ module.exports = {
   },
   watchContent: async (req, res, next) => {
     try {
-      res.status(200).send({
-        data: [{ message: "watch content" }],
-        message: "Successfull",
-        isError: false,
-        error: {},
-      });
+      const path = "assets/videos/movie.mp4";
+      console.log(path)
+      const stat = fs.statSync(path);
+      console.log(stat)
+      const fileSize = stat.size;
+      const range = req.headers.range;
+      if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunksize = end - start + 1;
+        const file = fs.createReadStream(path, { start, end });
+        const head = {
+          "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+          "Accept-Ranges": "bytes",
+          "Content-Length": chunksize,
+          "Content-Type": "video/mp4",
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+      } else {
+        const head = {
+          "Content-Length": fileSize,
+          "Content-Type": "video/mp4",
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
+      }
+      // res.status(200).send({
+      //   data: [{ message: "watch content" }],
+      //   message: "Successfull",
+      //   isError: false,
+      //   error: {},
+      // });
     } catch (error) {
+      console.log(error)
       res
         .status(400)
         .send({ data: [], message: "ERROR", isError: true, error });
